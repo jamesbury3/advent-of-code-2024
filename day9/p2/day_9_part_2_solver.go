192849b5df1f8f72774879a365684cfbb768d033
James Bury <jamesbury77@gmail.com>
2025-05-06 22:15:36 -0400

    feat: solves d9p2

--- day9/p2/day_9_part_2_solver.go
+++ day9/p2/day_9_part_2_solver.go
@@ -1,8 +1,113 @@
 package d9p2
 
+import (
+	"slices"
+	"strconv"
+)
+
 type Day9Part2Solver struct{}
 
 func (solver *Day9Part2Solver) Solve(lines []string) (string, error) {
 
-	return "", nil
+	line := lines[0]
+	space := cleanInput(line)
+
+	prev := ""
+	count := 0
+	visited := []string{}
+	for i := len(space) - 1; i >= 0; i-- {
+		curr := space[i]
+		if prev == "" && curr != "." {
+			prev = curr
+			count = 0
+		}
+		if curr == prev {
+			count += 1
+			continue
+		}
+		if slices.Contains(visited, prev) {
+			curr, prev, count = updateCounters(curr)
+			continue
+		}
+		startingIndex := findStartingIndex(space, count)
+		if startingIndex == -1 || startingIndex >= i {
+			visited = append(visited, prev)
+			curr, prev, count = updateCounters(curr)
+			continue
+		}
+		for j := range count {
+			space[startingIndex+j] = prev
+			space[i+1+(j)] = "."
+		}
+		visited = append(visited, prev)
+		curr, prev, count = updateCounters(curr)
+	}
+
+	sum := 0
+	for i, v := range space {
+		if v == "." {
+			continue
+		}
+		vInt, err := strconv.Atoi(v)
+		if err != nil {
+			return "", err
+		}
+		sum += i * vInt
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func findStartingIndex(space []string, length int) int {
+	count := 0
+	for i, v := range space {
+		if v == "." {
+			count += 1
+		} else {
+			count = 0
+		}
+		if count == length {
+			return i + 1 - length
+		}
+	}
+	return -1
+}
+
+func cleanInput(line string) []string {
+	fileId := -1
+	space := []string{}
+
+	for i, r := range line {
+		size := runeToInt(r)
+		var charToAdd string
+		if i%2 == 0 {
+			fileId += 1
+			charToAdd = strconv.Itoa(fileId)
+		} else {
+			charToAdd = "."
+		}
+		for range size {
+			space = append(space, charToAdd)
+		}
+	}
+
+	return space
+}
+
+func runeToInt(r rune) int {
+	return int(r - '0')
+}
+
+// updateCounters returns curr, prev, count based on whether curr is "."
+func updateCounters(curr string) (string, string, int) {
+	var prev string
+	var count int
+	if curr == "." {
+		prev = ""
+		count = 0
+	} else {
+		prev = curr
+		count = 1
+	}
+	return curr, prev, count
 }
--- day9/p2/day_9_part_2_solver_test.go
+++ day9/p2/day_9_part_2_solver_test.go
@@ -1,8 +1,8 @@
 package d9p2
 
 import (
-    "advent-of-code-2024/core"
-    "testing"
+	"advent-of-code-2024/core"
+	"testing"
 )
 
 func TestDay9Part2Solver_Solve_InputExample(t *testing.T) {
@@ -17,7 +17,7 @@ func TestDay9Part2Solver_Solve_InputExample(t *testing.T) {
     }{
         {
             name: "should return correct answer for input example",
-            want: "PLACEHOLDER",
+            want: "2858",
             fields: fields{
                 daySolverDelegate: &Day9Part2Solver{},
             },
@@ -52,7 +52,7 @@ func TestDay9Part2Solver_Solve_Input(t *testing.T) {
     }{
         {
             name: "should return correct answer for input",
-            want: "PLACEHOLDER",
+            want: "6547228115826",
             fields: fields{
                 daySolverDelegate: &Day9Part2Solver{},
             },
@@ -60,7 +60,6 @@ func TestDay9Part2Solver_Solve_Input(t *testing.T) {
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
-			t.Skip()
             solver := &core.DaySolver{
                 DaySolverDelegate: tt.fields.daySolverDelegate,
             }
@@ -75,3 +74,48 @@ func TestDay9Part2Solver_Solve_Input(t *testing.T) {
         })
     }
 }
+
+func Test_findStartingIndex(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		space  []string
+		length int
+		want   int
+	}{
+		{
+			space: []string{".",".",".","."},
+			length: 3,
+			want: 0,
+		},
+		{
+			space: []string{"1",".",".","."},
+			length: 3,
+			want: 1,
+		},
+		{
+			space: []string{".",".",".","1"},
+			length: 3,
+			want: 0,
+		},
+		{
+			space: []string{"4","5",".","1"},
+			length: 3,
+			want: -1,
+		},
+		{
+			space: []string{"4","5",".",".",".","1"},
+			length: 3,
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := findStartingIndex(tt.space, tt.length)
+			if got != tt.want {
+				t.Errorf("findStartingIndex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
